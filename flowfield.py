e406f7de7d946fbf78dee0d64f9834611d47f263
Johan <johanfendi0112@gmail.com>
2024-10-06 20:57:01 +0200

    First Implemenation of kernels

--- flowfield.py
+++ flowfield.py
@@ -7,50 +7,50 @@ class Cell:
     path = "path"
     wall = "wall"
     source = "source"
-    def __init__(self, width, x_pos, y_pos, color, acceleration_x = 0, acceleration_y = 0 , dist_to_source = 0):
+    wall_cost = 10000
+    def __init__(self, width, y_cord, x_cord):
         self.width = width
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.color = color
+        self.x_cord = x_cord
+        self.y_cord = y_cord
+        self.x_vector = 0
+        self.y_vector = 0
+        self.cost = 0
+        self.color = (255,255,255)
         self.type = Cell.path
-        self.acceleration_x = acceleration_x
-        self.acceleration_y = acceleration_y
-        self.dist_to_source = dist_to_source
 
 
     def draw(self, window):
-        start_x = self.x_pos * self.width
-        start_y = self.y_pos * self.width
-        try:
-            pg.draw.rect(window, self.color, (start_y, start_x, self.width, self.width))
-        except ValueError:
-            print(self.color)
+        start_x = self.x_cord * self.width
+        start_y = self.y_cord * self.width
+        pg.draw.rect(window, self.color, (start_x, start_y, self.width, self.width))
+        
     
-
     def change_type(self, draw_walls_mode):
         if draw_walls_mode: 
             self.type = Cell.wall
             self.color = (0,0,0)
+            self.cost = Cell.wall_cost
         else:
             self.type = Cell.path
 
 
 class Flow_field:
+    normalized_vec = 0.707
     def __init__(self, map_width, map_height, cell_width, source_x, source_y):
         self.map_width = map_width
         self.map_height = map_height
         self.cell_width = cell_width
-        self.source_x = source_x 
-        self.source_y = source_y 
         self.grid = self.create_grid()
+        self.source = self.grid[source_y][source_x]
         self.grid[source_y][source_x].type = Cell.source
+
 
     def create_grid(self):
         grid = [[] for _ in range(self.map_height)]
 
         for y_cord in range(self.map_height):
             for x_cord in range(self.map_width):
-                cell = Cell(self.cell_width, y_cord, x_cord, (255,255,255))
+                cell = Cell(self.cell_width, y_cord, x_cord)
                 grid[y_cord].append(cell)
         
         return grid 
@@ -61,24 +61,101 @@ class Flow_field:
                 cell.draw(window)
     
 
-    def compute_distances(self):
+    def generate_cost_field(self):
         queue = deque()
         visited = set()
-        queue.append([0, self.source_y, self.source_x])
-        visited.add((self.source_y, self.source_x))
+        queue.append([0, self.source.y_cord, self.source.x_cord])
+        visited.add((self.source.y_cord, self.source.x_cord))
 
         while len(queue) > 0:
             dist, y_cord, x_cord = queue.popleft()
             cell = self.grid[y_cord][x_cord]
-            cell.dist_to_source = dist
+            cell.cost = dist
 
-            for neighbour in self.get_neighbours(dist, y_cord, x_cord, visited):
+            for neighbour in self.get_neighs_bfs(dist, y_cord, x_cord, visited):
                 queue.append(neighbour)
         
         self.get_cell_colours(visited)
+    
+
+    def compute_vector_field(self):
+        self.apply_kernel(self.min_neighbour_kernel)
+
+        #self.apply_kernel(self.avg_neighbour_kernel)  #One needs to make a copy of the min_neigh_vector field first, then one can change 
 
 
-    def get_neighbours(self, dist, y_cord, x_cord, visited):
+    def apply_kernel(self, kernel):
+        len_row = len(self.grid[0])
+
+        for y_cord in range(len(self.grid)):
+            for x_cord in range(len_row):
+                cell = self.grid[y_cord][x_cord]
+                if cell.type == Cell.wall or cell == self.source:
+                    cell = self.grid[y_cord][x_cord]
+                    cell.x_vector = 0
+                    cell.y_vector = 0
+                    continue
+
+                kernel(y_cord, x_cord)
+    
+
+    def min_neighbour_kernel(self, y_cord, x_cord):
+        min_neighbour = None
+        min_cost = float('inf')
+        
+        for y_cord2 in range(y_cord-1, y_cord+2):
+            for x_cord2 in range(x_cord-1, x_cord+2):
+                if (not (0 <= y_cord2 < self.map_height) or not (0 <= x_cord2 < self.map_width) or 
+                    (y_cord2 == y_cord and x_cord == x_cord2)):
+                    continue
+                    
+                neigh_cell = self.grid[y_cord2][x_cord2]
+
+                diff_x = neigh_cell.x_cord - x_cord
+                diff_y = neigh_cell.y_cord - y_cord
+                assert -1 <= diff_x <= 1 and -1 <= diff_y <= 1, f"NEIGH_CELL_ERROR" 
+                if neigh_cell.cost < min_cost:
+                    min_neighbour = neigh_cell
+                    min_cost = neigh_cell.cost
+        
+
+        x_vector = min_neighbour.x_cord - x_cord
+        y_vector = min_neighbour.y_cord - y_cord
+        if abs(x_vector) == 1 and abs(y_vector) == 1:
+            x_vector = Flow_field.normalized_vec * x_vector
+            y_vector = Flow_field.normalized_vec * y_vector
+
+
+        cell = self.grid[y_cord][x_cord]
+        cell.x_vector = x_vector
+        cell.y_vector = y_vector
+
+        assert -1 <= abs(x_vector) <= 1 and -1 <= abs(y_vector) <= 1, f"VECTORERROR"
+    
+    def avg_neighbour_kernel(self, y_cord, x_cord):
+
+        cell = self.grid[y_cord][x_cord]
+        y_vector = 0
+        x_vector = 0
+        for y_cord2 in range(y_cord-1, y_cord+2):
+            for x_cord2 in range(x_cord-1, x_cord+2):
+                if (not (0 <= y_cord2 < self.map_height) or not (0 <= x_cord2 < self.map_width)): #or 
+                    #(y_cord2 == y_cord and x_cord == x_cord2)):
+                    continue
+
+                cell = self.grid[y_cord2][x_cord2]
+                x_vector += cell.x_vector
+                y_vector += cell.y_vector
+        
+        x_vector /= 8
+        y_vector /= 8
+        cell.x_vector = x_vector
+        cell.y_vector = y_vector
+
+        assert -1 <= abs(x_vector) <= 1 and -1 <= abs(y_vector) <= 1, f"VECTORERROR"
+
+               
+    def get_neighs_bfs(self, dist, y_cord, x_cord, visited):
         neighbours = []
         if y_cord > 0 and (y_cord-1, x_cord) not in visited and self.grid[y_cord-1][x_cord].type == Cell.path:
             neighbours.append([dist+1, y_cord-1, x_cord])
@@ -112,14 +189,14 @@ class Flow_field:
                     continue
 
                 if (y_cord, x_cord) not in visited:
-                    cell.color = (0,0,255)
+                    cell.color = (0,0,155)
                     continue
 
 
                 k = (self.map_height + self.map_height) * 1.5
-                redness = max(0, (-255/k) * cell.dist_to_source + 255)
-                blueness = min(255, 255/k * cell.dist_to_source )
-                cell.color = (redness, 100, blueness)
+                redness = max(0, (-255/k) * cell.cost + 255)
+                blueness = min(155, 155/k * cell.cost)
+                cell.color = (redness, 0, blueness)
 
                 
     
--- main.py
+++ main.py
@@ -2,17 +2,28 @@ import pygame as pg
 from flowfield import *
 
 
-
-cell_width  = 20
+cell_width = 20
 map_width = 60
 map_height = 30
 window_height = cell_width * map_height
 window_width = cell_width * map_width
-source_x = 10
-source_y = 10
-fps = 60
+source_x = 30
+source_y = 15
 
+fps = 60
 draw_mode = True
+
+
+def wall_placement(flow_field, draw_mode):
+    mouse_buttons = pg.mouse.get_pressed() 
+    if mouse_buttons[0]:
+        pos = pg.mouse.get_pos()
+        x_cord, y_cord = pos[0] // cell_width, pos[1] // cell_width
+        if (0 <= x_cord < map_width) and (0 <= y_cord < map_height):
+            cell = flow_field.grid[y_cord][x_cord]
+            if cell.type != Cell.source:
+                cell.change_type(draw_mode)
+
 
 def main(clock, fps, flow_field, window, draw_mode):
     running = True
@@ -28,39 +39,39 @@ def main(clock, fps, flow_field, window, draw_mode):
                     running = False
 
 
-        mouse_buttons = pg.mouse.get_pressed()
-        if mouse_buttons[0]:
-            try:
-                pos = pg.mouse.get_pos()
-                x_cord, y_cord = pos[0] // cell_width, pos[1] // cell_width
-                cell = flow_field.grid[y_cord][x_cord]
-                if cell.type != Cell.source:
-                    cell.change_type(draw_mode)
-            except IndexError:
-                print(f'INDEXERROR: Y = {y_cord} X = {x_cord}')
-
-
-        flow_field.compute_distances()
+        wall_placement(flow_field, draw_mode)
+        flow_field.generate_cost_field()
         flow_field.draw(window)
-        clock.tick(fps)
-                
-        draw_state = "ON" if draw_mode else "OFF"
-        erase_state = "OFF" if draw_mode else "ON"
-
-
-        caption_strings =   ["Left click to draw", f"Draw: {draw_state}", 
-                            f"Eraser: {erase_state}", "PRESS C to switch between draw and erase",
+        
+        draw = "ON" if draw_mode else "OFF"
+        erase = "OFF" if draw_mode else "ON"
+        caption_strings =   ["Left click to draw", f"Draw: {draw}", 
+                            f"Eraser: {erase}", "PRESS C to switch between draw and erase",
                             "PRESS Q to quit"  
                             ]
 
         pg.display.set_caption(" | ".join(caption_strings))
         pg.display.update()
+        clock.tick(fps)
+
+
+def print_vecs1(flow_field):
+    for row in flow_field.grid:
+        for cell in row:
+            print(round(cell.y_vector,2), round(cell.x_vector,2), end = "  ")
+        print()
+
 
 
 if __name__ == "__main__":  
+    
     pg.init()
     clock = pg.time.Clock()
     window = pg.display.set_mode((window_width, window_height))
     flow_field = Flow_field(map_width, map_height, cell_width, source_x, source_y)
-    flow_field.compute_distances()
     main(clock, fps, flow_field, window, draw_mode)
+    flow_field.compute_vector_field()
+
+    print_vecs1(flow_field)
+
+ 
